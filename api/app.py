21ac32223c0e593d8b9a5c531e1a8ce2985cd07e
Erika Pogorelc <erika@serox.si>
2019-09-28 19:17:20 +0200

    Adding the redis configuration

--- api/app.py
+++ api/app.py
@@ -1,32 +1,41 @@
-from flask import Flask, jsonify
-from config import *
+from flask import Flask
+from flask_migrate import Migrate
+from flask_redis import FlaskRedis
+
+from .config import *
 from .models.database import db, ma
+from .controllers import v1_routes, error_routes
+from .utils.request_utils import RequestFormatter
+
+import logging
 
 # App init
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
 # Database and Marshmallow init
+# Marshmallow is a serializer
 db.init_app(app)
 ma.init_app(app)
 
+# Data migrations setup (not sure about this)
+migrate = Migrate(app, db, directory=DevelopmentConfig.MIGRATION_DIR)
 
-# Controllers
-@app.route('/')
-def index():
-    return_obj = {'msg': 'welcome'}
-    return jsonify(return_obj)
+# Blueprint register
+# We added this so we can change the app layout structure
+app.register_blueprint(v1_routes.bp, url_prefix='/api/v1')
+app.register_blueprint(error_routes.bp)
 
-@app.route("/grab_and_save", methods=['POST'])
-def grab_and_save():
-    return jsonify({'msg': 'you need to post some data'})
+# setting logger
+formatter = RequestFormatter(
+    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+    '%(levelname)s in %(module)s: %(message)s'
+)
+handler = logging.FileHandler('access.log')
+handler.setFormatter(formatter)
+logger = logging.getLogger('werkzeug')
+logger.addHandler(handler)
+# app.logger.addHandler(handler)
 
-
-@app.route("/last", methods=['GET'])
-def get_last_record():
-    return None
-
-
-if __name__ == '__main__':
-    # Will make the server available externally as well
-    app.run(host='0.0.0.0')
+# Redis
+redis_client = FlaskRedis(app)

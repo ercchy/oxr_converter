8c7deaf84e87d19c7964210c771de317db0f92da
Erika Pogorelc <erika@serox.si>
2019-09-28 19:10:51 +0200

    Adds the initial data model

---
+++ api/models/convert_data.py
@@ -0,0 +1,39 @@
+from sqlalchemy import func, DateTime
+
+from .database import db, ma
+
+
+class ConvertData(db.Model):
+    # Self incrementing PK
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Passed data
+    currency_code = db.Column(db.String(3), nullable=False)
+    requested_amount = db.Column(db.DECIMAL(precision=8), nullable=False)
+
+    # Retrived data
+    oxr_price = db.Column(db.DECIMAL, nullable=False)
+
+    # Calculated ammount
+    final_amount = db.Column(db.DECIMAL(precision=8), nullable=False)
+
+    # Metadata
+    date_created = db.Column(DateTime(timezone=True), server_default=func.now())
+    date_updated = db.Column(DateTime(timezone=True), server_default=func.now())
+
+    # def __init__(self, currency_code, requested_amount, oxr_price, final_amount):
+    #     currency_code = self.currency_code
+    #     requested_amount = self.requested_amount
+    #     oxr_price = self.oxr_price
+    #     final_amount = self.final_amount
+
+
+    def __repr__(self):
+        return f'<ID {self.id}>'
+
+# RequestInfo schema
+class ConvertDataSchema(ma.Schema):
+    class Meta:
+        fields = ('currency_code', 'requested_amount', 'oxr_price', 'final_amount', 'date_created', 'date_updated')
+# Init schema
+convert_data_schema = ConvertDataSchema()

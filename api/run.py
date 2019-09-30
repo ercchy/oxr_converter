6e2dfb093986d598af2d674da1367b4757573ee8
Erika Pogorelc <erika@serox.si>
2019-09-30 08:31:19 +0200

    Renames app.py to run, for better clarity

--- api/run.py
+++ api/run.py
@@ -1,43 +1,43 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from .utils import middleware
 from .config import *
 from .models.database import db, ma
 from .cache.redis import redis
 from .controllers import v1_routes, error_routes
 from .utils.request_utils import RequestFormatter
+from .utils.middleware import LoggingMiddleware
 
 import logging
 
 # App init
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-app.wsgi_app = middleware.LoggingMiddleware(app.wsgi_app)
+application = Flask(__name__)
+application.config.from_object(DevelopmentConfig)
+application.wsgi_app = LoggingMiddleware(application.wsgi_app)
 
 # Database and Marshmallow init
 # Marshmallow is a serializer
-db.init_app(app)
-ma.init_app(app)
+db.init_app(application)
+ma.init_app(application)
 
 # Data migrations setup (not sure about this)
-migrate = Migrate(app, db, directory=DevelopmentConfig.MIGRATION_DIR)
+migrate = Migrate(application, db, directory=DevelopmentConfig.MIGRATION_DIR)
 
 # Redis
-redis.init_app(app)
+redis.init_app(application)
 
 # Blueprint register
-# We added this so we can change the app layout structure
-app.register_blueprint(v1_routes.bp, url_prefix='/api/v1')
-app.register_blueprint(error_routes.bp)
+# We added this so we can change the application layout structure
+application.register_blueprint(v1_routes.bp, url_prefix='/api/v1')
+application.register_blueprint(error_routes.bp)
 
 # setting logger
 formatter = RequestFormatter(
     '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
     '%(levelname)s in %(module)s: %(message)s'
 )
-handler = logging.FileHandler('access.log')
+handler = logging.FileHandler(DevelopmentConfig.LOG_FILE)
 handler.setFormatter(formatter)
 logger = logging.getLogger('werkzeug')
 logger.addHandler(handler)
-app.logger.addHandler(handler)
+application.logger.addHandler(handler)

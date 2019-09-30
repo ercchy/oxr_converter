f9d800df128da642fda05248994626efb05b72f7
Erika Pogorelc <erika@serox.si>
2019-09-30 03:42:15 +0200

    Makes use of the events in database

--- api/models/convert_data.py
+++ api/models/convert_data.py
@@ -1,7 +1,10 @@
+import simplejson as json
+
 from sqlalchemy import func, DateTime
 from sqlalchemy.event import listens_for
 
-from ..cache import redis
+from ..utils.data_utils import datetime_converter
+from ..cache.redis import redis
 from .database import db, ma
 
 import logging
@@ -27,20 +30,30 @@ class ConvertData(db.Model):
     date_updated = db.Column(DateTime(timezone=True), server_default=func.now())
     request_id = db.Column(db.String(100), nullable=False)
 
+    @property
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
     def __repr__(self):
         return f'<ID {self.request_id}>'
 
-# RequestInfo schema
-class ConvertDataSchema(ma.Schema):
-    class Meta:
-        fields = ('currency_code', 'requested_amount', 'oxr_price', 'final_amount', 'date_created', 'date_updated')
-# Init schema
-convert_data_schema = ConvertDataSchema()
 
+# RequestInfo schema
+class ConvertDataSchema(ma.ModelSchema):
+    class Meta:
+        model = ConvertData
+
+converted_data_schema = ConvertDataSchema()
+converted_datas_schema = ConvertDataSchema(many=True)
 
 @listens_for(ConvertData, 'after_insert', propagate=True)
 def after_insert_function(mapper, connection, target):
-    logger.error('Signal was fired: {}', target.id)
-    print("work jeba")
     assert target.id is not None
-    redis.set('{}:{}:{}'.format(target.currency_code, target.request_id, 1), 1)
+    logger.info('Event after_insert for ConvertData model was fired: {}'.format(target.request_id))
+    # Surely there is a better way than this?
+    cleaned_data = target.to_dict
+    json.dumps(cleaned_data, default = datetime_converter)
+    jsoned_entry = json.dumps(cleaned_data, use_decimal=True, default = datetime_converter)
+    # TODO: Error handle
+    redis.zadd('{}'.format(target.currency_code), {jsoned_entry: target.id})
+    redis.zadd('operation', {jsoned_entry: target.id})
